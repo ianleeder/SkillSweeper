1b21b44a661fab32aba47a82b83f5cbb894fb7a5
Ian Leeder <i_leeder@hotmail.com>
2016-09-26 14:07:32 +1000

    Set up basic field drawing, testing context translation

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// Author: Ian Leeder
+// Date: 07 November 2014
 
-var cellSize = 30;
+// CONSTANTS
+var CELL_SIZE = 30;
 
-// Define header region
+
+// VARIABLES
+
 var header = new Box(0, 0, 200, 150);
 
 var gameGrid;
@@ -14,36 +19,51 @@ var difficulty = expertDifficulty;
 
 
 // Get the canvas context
-var canvas = document.getElementById('myCanvas');
-canvas.width = difficulty.x * cellSize;
-canvas.height = (difficulty.y * cellSize) + headerHeight;
+var canvas = document.getElementById('skillSweeperCanvas');
+var ctx = canvas.getContext('2d');
 
-var canvasContext = canvas.getContext('2d');
-
-canvasContext.fillStyle = "#000000";
-	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-
+init();
+drawField();
 
 function Box(x, y, width, height) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
 }
 
 function drawField() {
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(0,0,canvas.width, canvas.height);
+
+	ctx.translate(10, 20);
+
 	for(var i=0;i<gameGrid.length;i++) {
 		for(var j=0;j<gameGrid[i].length;j++) {
-			if(gameGrid[i][j]) {
-				canvasContext.drawImage(gameGrid[i][j].image, TILE_SIZE*(i+1), TILE_SIZE*(j+1));
-			}
+			ctx.fillStyle = getRandomColor();
+			ctx.fillRect(i*CELL_SIZE, j*CELL_SIZE, CELL_SIZE, CELL_SIZE);
 		}
 	}
+
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 function init() {
-   gameGrid = new Array(difficulty.x);
-// Create empty array for grid
+	canvas.width = difficulty.x * CELL_SIZE;
+	canvas.height = (difficulty.y * CELL_SIZE);// + headerHeight;
+
+	gameGrid = new Array(difficulty.x);
+	// Create empty array for grid
 	for(var i = 0; i < gameGrid.length; i++) {
 		gameGrid[i] = new Array(difficulty.y);
+	}
 }
